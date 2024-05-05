cb62f8dacb4aadc1761967b754fe11ab65cfc00f
jokowaluyo04 <12250112564@students.uin-suska.ac.id>
2024-05-05 23:50:40 +0700

    Initial commit

---
+++ app/src/main/java/com/example/apklogindanregister/Api/ApiInterface.java
@@ -0,0 +1,27 @@
+package com.example.apklogindanregister.Api;
+
+
+import com.example.apklogindanregister.Login.Login;
+import com.example.apklogindanregister.Register.Register;
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
+public interface ApiInterface {
+    @FormUrlEncoded
+    @POST("login.php")
+    Call<Login> loginResponse(
+            @Field("username") String username,
+            @Field("password") String password
+    );
+
+    @FormUrlEncoded
+    @POST("register.php")
+    Call<Register> registerResponse(
+            @Field("username") String username,
+            @Field("password") String password,
+            @Field("name") String name
+    );
+}
